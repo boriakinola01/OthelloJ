e9cd5d28db582a647f1d749fe1fc88ecc7d4c71f
Bori <toluwabori,akinola@ucdconnect.ie>
2021-10-25 23:31:58 +0100

    Starting with the board

--- src/Player.java
+++ src/Player.java
@@ -9,7 +9,31 @@ public class Player {
         this.colour = colour;
     }
 
-    public void move(char[] input){
+    public void move(char[] input, Board b){
+
+    }
+
+
+    public boolean checkMove(char[] input){
+
+        return false;
+    }
+
+    public boolean isNotEmpty(char[] input){
+
+        return false;
+    }
+
+    public boolean validMove(char color, int dRow, int dCol, int row, int col){
+        char opp;
+
+        if(color == 'B')
+            opp = 'W';
+        else if(color == 'W')
+            opp = 'B';
+
+
+        return false;
 
     }
 
