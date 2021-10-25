9b730e22de4a96f2eafc700b0710b38394d71189
Bori <toluwabori,akinola@ucdconnect.ie>
2021-10-25 22:32:03 +0100

    Starting with the board

--- src/Player.java
+++ src/Player.java
@@ -1,4 +1,16 @@
 public class Player {
 
+    public int score;
+    public String name;
+    public char colour;
+
+    public Player(String name, char colour) {
+        this.name = name;
+        this.colour = colour;
+    }
+
+    public void move(char[] input){
+
+    }
 
 }
